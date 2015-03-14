3a347ab36f79bded8118b476baec068ef7cb014f
David Mandel <MandelDL@gmail.com>
2015-03-14 15:48:03 -0400

    update events model

--- db/migrate/20150314191359_create_events.rb
+++ db/migrate/20150314191359_create_events.rb
@@ -1,7 +1,16 @@
 class CreateEvents < ActiveRecord::Migration
   def change
     create_table :events do |t|
-
+		# a string column called name
+		t.string :name
+		# a datetime column called from
+		t.datetime :from
+		# a datetime column called to
+		t.datetime :to
+		# a string column called location
+		t.string :location
+		# a references column to the Day model
+		t.references :day
       t.timestamps
     end
   end
